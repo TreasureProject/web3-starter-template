bb3c568e18eb3a8744edd44dd35dc2e0ee1c585d
jcheese1 <jcheese@treasure.lol>
2025-08-11 12:44:40 +0900

    initial commit

---
+++ app/env.server.ts
@@ -0,0 +1,30 @@
+import { env } from 'cloudflare:workers';
+import { type } from 'arktype';
+
+export const envType = type({
+  PUBLIC_ENVIRONMENT: "'development' | 'staging' | 'production'",
+  SECRET: 'string',
+});
+
+export function getEnv() {
+  return Object.fromEntries(
+    Object.entries(env).filter(([key]) => key.startsWith('PUBLIC_')),
+  ) as PublicEnv;
+}
+export type Env = typeof envType.infer;
+
+export type PublicEnv = {
+  [K in keyof Env as K extends `PUBLIC_${string}` ? K : never]: Env[K];
+};
+
+export function init() {
+  const parsed = envType(env);
+
+  if (parsed instanceof type.errors) {
+    console.error('❌ Invalid environment variables:', parsed.summary);
+
+    throw new Error('Invalid environment variables', { cause: parsed.summary });
+  }
+}
+
+export type PUBLIC_ENV = ReturnType<typeof getEnv>;
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { cloudflare } from '@cloudflare/vite-plugin';
+import { reactRouter } from '@react-router/dev/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import { iconsSpritesheet } from 'vite-plugin-icons-spritesheet';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig(({ mode }) => {
+  console.log(mode, process.env.CLOUDFLARE_ENV);
+  return {
+    build: {
+      assetsInlineLimit(filePath) {
+        return !filePath.endsWith('icon.svg');
+      },
+    },
+    plugins: [
+      cloudflare({ viteEnvironment: { name: 'ssr' } }),
+      tailwindcss(),
+      reactRouter(),
+      tsconfigPaths(),
+      iconsSpritesheet({
+        withTypes: true,
+        inputDir: 'icons',
+        outputDir: './app/icons',
+        fileName: 'icon.svg',
+        formatter: 'biome',
+      }),
+    ],
+  };
+});
